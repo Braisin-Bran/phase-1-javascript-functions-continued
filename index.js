b8acbab7a8a61bb1a069d78606fee8ce5a49ec34
Braylan Dodson <braylan1005@gmail.com>
2022-05-16 20:30:48 -0400

    commit tree ab07f53a974ba0bcde7456e17fb328b87b058ecf

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    
+    return(`This Saturday, I want to ${activity}!`);
+}
+function mondayWork(activity = "go to the office"){
+    
+    return(`This Monday, I will ${activity}.`);
+}
+function wrapAdjective(input = "special"){
+    const flair = function(input2 ="*"){
+        return(`You are ${input}${input2}${input}!`);
+    }
+return(flair);
+}
